7ae16557c703b6636881fa05179dd513452d89e3
Jordan De Mello <jormello@amazon.com>
2021-06-15 14:39:29 -0700

    Add delete handler tests. Revert template global timeout to 60

--- statemachine/src/test/java/com/amazonaws/stepfunctions/cloudformation/statemachine/DeleteHandlerTest.java
+++ statemachine/src/test/java/com/amazonaws/stepfunctions/cloudformation/statemachine/DeleteHandlerTest.java
@@ -1,12 +1,10 @@
 package com.amazonaws.stepfunctions.cloudformation.statemachine;
 
-import com.amazonaws.services.stepfunctions.model.DeleteStateMachineRequest;
 import com.amazonaws.services.stepfunctions.model.DescribeStateMachineRequest;
 import com.amazonaws.services.stepfunctions.model.DescribeStateMachineResult;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import software.amazon.cloudformation.proxy.HandlerErrorCode;
@@ -14,11 +12,8 @@ import software.amazon.cloudformation.proxy.OperationStatus;
 import software.amazon.cloudformation.proxy.ProgressEvent;
 import software.amazon.cloudformation.proxy.ResourceHandlerRequest;
 
-import java.util.List;
 import java.util.function.Function;
 
-import static com.amazonaws.stepfunctions.cloudformation.statemachine.MetricsLoggingKeys.OPERATION_FAILURE;
-import static com.amazonaws.stepfunctions.cloudformation.statemachine.MetricsLoggingKeys.OPERATION_SUCCESS;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(MockitoExtension.class)
@@ -26,7 +21,8 @@ public class DeleteHandlerTest extends HandlerTestBase {
 
     private DeleteHandler handler = new DeleteHandler();
 
-    private final ArgumentCaptor<String> argumentCaptor = ArgumentCaptor.forClass(String.class);
+    private CallbackContext callbackContext;
+
     private ResourceHandlerRequest<ResourceModel> request;
 
     @BeforeEach
@@ -39,17 +35,18 @@ public class DeleteHandlerTest extends HandlerTestBase {
                         .build()
                 )
                 .build();
+
+        callbackContext = CallbackContext.builder().build();
     }
 
     @Test
-    public void testSuccess_initialCall_returnsInProgress() {
+    public void testSuccess_initialCall_returnsInProgress_withDeletionStarted() {
         ProgressEvent<ResourceModel, CallbackContext> response = handler.handleRequest(
                 proxy,
                 request,
                 null,
                 logger);
 
-        assertThat(response).isNotNull();
         assertThat(response.getStatus()).isEqualTo(OperationStatus.IN_PROGRESS);
         assertThat(response.getCallbackContext()).isNotNull();
         assertThat(response.getCallbackContext().isDeletionStarted()).isTrue();
@@ -63,15 +60,14 @@ public class DeleteHandlerTest extends HandlerTestBase {
     public void testSuccess_deletionStarted_andStillDeleting_returnsInProgress() {
         Mockito.when(proxy.injectCredentialsAndInvoke(Mockito.any(DescribeStateMachineRequest.class), Mockito.any(Function.class))).thenReturn(new DescribeStateMachineResult());
 
+        callbackContext.setDeletionStarted(true);
+
         ProgressEvent<ResourceModel, CallbackContext> response = handler.handleRequest(
                 proxy,
                 request,
-                CallbackContext.builder()
-                        .deletionStarted(true)
-                        .build(),
+                callbackContext,
                 logger);
 
-        assertThat(response).isNotNull();
         assertThat(response.getStatus()).isEqualTo(OperationStatus.IN_PROGRESS);
         assertThat(response.getCallbackContext()).isNotNull();
         assertThat(response.getCallbackContext().isDeletionStarted()).isTrue();
@@ -85,15 +81,14 @@ public class DeleteHandlerTest extends HandlerTestBase {
     public void testSuccess_deletionStarted_andCompletes_returnsSuccess() {
         Mockito.when(proxy.injectCredentialsAndInvoke(Mockito.any(DescribeStateMachineRequest.class), Mockito.any(Function.class))).thenThrow(stateMachineDoesNotExistException);
 
+        callbackContext.setDeletionStarted(true);
+
         ProgressEvent<ResourceModel, CallbackContext> response = handler.handleRequest(
                 proxy,
                 request,
-                CallbackContext.builder()
-                        .deletionStarted(true)
-                        .build(),
+                callbackContext,
                 logger);
 
-        assertThat(response).isNotNull();
         assertThat(response.getStatus()).isEqualTo(OperationStatus.SUCCESS);
         assertThat(response.getCallbackContext()).isNull();
         assertThat(response.getCallbackDelaySeconds()).isEqualTo(0);
@@ -109,7 +104,6 @@ public class DeleteHandlerTest extends HandlerTestBase {
         final ProgressEvent<ResourceModel, CallbackContext> response
                 = handler.handleRequest(proxy, request, null, logger);
 
-        assertThat(response).isNotNull();
         assertThat(response.getStatus()).isEqualTo(OperationStatus.FAILED);
     }
 
@@ -120,7 +114,6 @@ public class DeleteHandlerTest extends HandlerTestBase {
         final ProgressEvent<ResourceModel, CallbackContext> response
                 = handler.handleRequest(proxy, request, null, logger);
 
-        assertThat(response).isNotNull();
         assertThat(response.getStatus()).isEqualTo(OperationStatus.FAILED);
         assertThat(response.getMessage()).isEqualTo(exception500.getMessage());
     }
@@ -132,22 +125,42 @@ public class DeleteHandlerTest extends HandlerTestBase {
         final ProgressEvent<ResourceModel, CallbackContext> response
                 = handler.handleRequest(proxy, request, null, logger);
 
-        assertThat(response).isNotNull();
         assertThat(response.getStatus()).isEqualTo(OperationStatus.FAILED);
         assertThat(response.getMessage()).isEqualTo(Constants.INTERNAL_FAILURE_MESSAGE);
     }
 
     @Test
-    public void testWhenArnMissing_throwsStateMachineDoesNotExist() {
+    public void testThrowsStateMachineDoesNotExist_whenArnMissing() {
         request.setDesiredResourceState(ResourceModel.builder().build());
 
         final ProgressEvent<ResourceModel, CallbackContext> response
                 = handler.handleRequest(proxy, request, null, logger);
 
-        assertThat(response).isNotNull();
         assertThat(response.getStatus()).isEqualTo(OperationStatus.FAILED);
         assertThat(response.getErrorCode()).isEqualTo(HandlerErrorCode.NotFound);
         assertThat(response.getMessage()).contains(Constants.STATE_MACHINE_DOES_NOT_EXIST_ERROR_MESSAGE);
     }
 
+    @Test
+    public void testThrowsStateMachineDoesNotExist_whenStateMachineDoesNotExist() {
+        Mockito.when(proxy.injectCredentialsAndInvoke(Mockito.any(DescribeStateMachineRequest.class), Mockito.any(Function.class))).thenThrow(stateMachineDoesNotExistException);
+
+        final ProgressEvent<ResourceModel, CallbackContext> response
+                = handler.handleRequest(proxy, request, null, logger);
+
+        assertThat(response.getStatus()).isEqualTo(OperationStatus.FAILED);
+        assertThat(response.getErrorCode()).isEqualTo(HandlerErrorCode.NotFound);
+        assertThat(response.getMessage()).contains(Constants.STATE_MACHINE_DOES_NOT_EXIST_ERROR_MESSAGE);
+    }
+
+    @Test
+    public void testThrowsException_whenDescribeStateMachineThrowsNotStateMachineDoesNotExistException() {
+        Mockito.when(proxy.injectCredentialsAndInvoke(Mockito.any(DescribeStateMachineRequest.class), Mockito.any(Function.class))).thenThrow(unknownException);
+
+        final ProgressEvent<ResourceModel, CallbackContext> response
+                = handler.handleRequest(proxy, request, null, logger);
+
+        assertThat(response.getStatus()).isEqualTo(OperationStatus.FAILED);
+        assertThat(response.getMessage()).isEqualTo(Constants.INTERNAL_FAILURE_MESSAGE);
+    }
 }
