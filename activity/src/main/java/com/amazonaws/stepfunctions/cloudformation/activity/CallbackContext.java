9a2f47ee9ac8f5530ea78e0a39302f820ec800e1
Hong Kun Tian <tiahong@amazon.com>
2024-04-15 17:15:20 -0700

    Revert "Add stabilization to Activity Delete handler (#51)"
    This reverts commit e698a38e4ae52c001052582d1c4b54b5d67dde1c.

--- activity/src/main/java/com/amazonaws/stepfunctions/cloudformation/activity/CallbackContext.java
+++ activity/src/main/java/com/amazonaws/stepfunctions/cloudformation/activity/CallbackContext.java
@@ -1,16 +1,12 @@
 package com.amazonaws.stepfunctions.cloudformation.activity;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import software.amazon.cloudformation.proxy.StdCallbackContext;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 @Builder
-public class CallbackContext extends StdCallbackContext {
-    private boolean isActivityDeletionStarted;
-    private int retryCount;
+public class CallbackContext {
+
 }
