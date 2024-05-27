1c96ce5b716e69dce18588fc3fdc12f46dc682be
Hong Kun Tian <tiahong@amazon.com>
2024-05-27 10:47:32 -0700

    Inherit StdCallbackContext for CallbackContext

--- statemachine/src/main/java/com/amazonaws/stepfunctions/cloudformation/statemachine/CallbackContext.java
+++ statemachine/src/main/java/com/amazonaws/stepfunctions/cloudformation/statemachine/CallbackContext.java
@@ -4,12 +4,13 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import software.amazon.cloudformation.proxy.StdCallbackContext;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class CallbackContext {
+public class CallbackContext extends StdCallbackContext {
     @Builder.Default
     private boolean deletionStarted = false;
 }
